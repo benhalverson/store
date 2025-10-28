3eae2d37fe00b3e88fa5650a6a6f1ca84c9f17f2
Ben Halverson <benhalverson33@gmail.com>
2025-10-28 02:19:45 -0700

    new icons

--- src/components/SearchBar.tsx
+++ src/components/SearchBar.tsx
@@ -1,3 +1,4 @@
+import { Icon } from "@magicons/react";
 import { useState } from "react";
 import { useNavigate } from "react-router-dom";
 
@@ -61,20 +62,7 @@ export function SearchBar({
       <button
         type="submit"
         className="px-4 py-2 bg-blue-500 hover:bg-blue-600 text-white rounded-r-lg transition-colors duration-200 focus:outline-none focus:ring-2 focus:ring-blue-500">
-        <svg
-          className="w-5 h-5"
-          fill="none"
-          stroke="currentColor"
-          viewBox="0 0 24 24"
-          xmlns="http://www.w3.org/2000/svg">
-          <title>Search</title>
-          <path
-            strokeLinecap="round"
-            strokeLinejoin="round"
-            strokeWidth={2}
-            d="M21 21l-6-6m2-5a7 7 0 11-14 0 7 7 0 0114 0z"
-          />
-        </svg>
+        <Icon src="@hero-MagnifyingGlass" />
       </button>
     </form>
   );
